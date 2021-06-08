088f552395e5dd0e4c0f764295572d9691fd69d8
Bima Pramudya Asaddulloh <pram.bima@gmail.com>
2021-06-08 12:31:45 +0700

    initial commit

---
+++ application/views/barang/form_barang.php
@@ -0,0 +1,30 @@
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<h4>Isikan data dengan lengkap</h4>
+				<form class="form-horizontal form-material" id="formBarang">
+					<div class="form-group">
+						<label class="col-md-12">Nama Barang</label>
+						<div class="col-md-12">
+							<input type="text" placeholder="Inputkan nama barang" class="form-control form-control-line form-user-input" name="nama_barang" id="nama_barang">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-12">Deskripsi</label>
+						<div class="col-md-12">
+							<textarea rows="5" class="form-control form-control-line form-user-input" name="deskripsi" id="deskripsi" placeholder="Ceritakan Barang"></textarea>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-12">
+							<input class="form-user-input" type="hidden" name="stok" id="stok" value="0">
+							<button class="btn btn-success" type="submit">Simpan Data Barang</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			
+		</div>
+	</div>
+</div>
